367e14edc83341fc54b66aba62aa5f6e42a86f58
Srinihitha Nalamachu <srinihitha.nalamachu@creditsafe.com>
2026-03-21 22:49:16 +0530

    added the css for layout

--- netflix_app/src/pages/Home/Home.jsx
+++ netflix_app/src/pages/Home/Home.jsx
@@ -3,6 +3,8 @@ import './Home.css'
 import hero_banner from '../../assets/hero_banner.jpg'
 import Navbar from '../../components/Navbar/Navbar'
 import hero_title from '../../assets/hero_title.png'
+import play_icon from '../../assets/play_icon.png'
+import info_icon from '../../assets/info_icon.png'
 const Home=()=>{
     return (
          <div className='home'> 
@@ -12,6 +14,13 @@ const Home=()=>{
      <div className="hero-caption">
         <img src={hero_title} alt="" className='caption-img'/>
        <p>Discovering his ties to a secret ancient order, a young man living in modern Istanbul embarks on a quest to save the city from an immortal enemy.</p>
+       <div className="hero-btns">
+        <button className='btn'>
+            <img src={play_icon} alt="">Play</img>  </button> 
+             <button className='btn dark btn'>
+            <img src={info_icon} alt="">More Info</img>
+        </button>
+       </div>
      </div>
          </div>
          </div>
